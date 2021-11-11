6621bc3e37ea70d9e1767f0f13ecae1809c28183
volkanustekidag <volkanustekidg@gmail.com>
2021-11-11 23:27:47 +0300

    http pratics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { PostService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http-start';
+export class AppComponent implements OnInit, OnDestroy {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = null;
+  private errorSub: Subscription;
+
+  constructor(private http: HttpClient, private postService: PostService) { }
+
+  ngOnInit() {
+    this.errorSub = this.postService.fethcPosts()
+      .subscribe(posts => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      }, error => {
+        this.isFetching = false;
+        this.error = error.message;
+      });
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.postService.createAndStorePost(postData.title, postData.content);
+    console.log(postData);
+
+  }
+
+  onFetchPosts() {
+    // Send Http request
+
+    this.postService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    })
+
+    this.isFetching = true;
+    this.postService.fethcPosts()
+      .subscribe(posts => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      }, error => {
+        this.isFetching = false;
+        this.error = error.message;
+        console.log(error);
+        
+      });;
+  }
+
+  onClearPosts() {
+    // Send Http request
+    this.postService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
+  }
+
+  onHandleError(){
+    this.error =null;
+  }
+
+  ngOnDestroy(){
+    this.errorSub.unsubscribe();
+  }
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { AuthInterceptorService } from './auth-interceptor.service';
+import { LoggingInterceptorService } from './logging-interceptor.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptorService, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
